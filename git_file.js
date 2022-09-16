d79ff25fdc5a92ad7e05d5161dd2ebb4184747fc
Stephanie <steph_war@yahoo.ca>
2022-09-16 11:18:44 -0700

    change to try to get conflict

--- git_file.js
+++ git_file.js
@@ -1,9 +1,13 @@
 //Hello Bootcamp class!!
 
-//make a change
+//this is another change to try to make a conflict
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 //asdasd
 =======
 //new branch change
 >>>>>>> refs/remotes/origin/main
+=======
+//new change
+>>>>>>> 869dae6 (change to try to get conflict)
